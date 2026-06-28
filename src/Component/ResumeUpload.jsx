d36a998ffe9a3e82b271a90ca549af588c951416
Jayesh <jayeshdhamal327@gmail.com>
2026-06-28 15:35:56 +0530

    folder structure is expanded

---
+++ src/Component/ResumeUpload.jsx
@@ -0,0 +1,8 @@
+// here we have to write the frontend code in order to uplad the resume 
+// basically we need the usestate here as screen will change asap we click on upload 
+
+import { useState } from "react";
+import {
+    uploadResume,
+    startInterview
+} from "../api/AIInterviewCopilot";
